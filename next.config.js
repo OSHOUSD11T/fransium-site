6d1e9097dc7b4a2c8a107ee07e9bc1ec90630dd7
OSHOUSD11T <oshomkudss112oi@gmail.com>
2025-07-17 19:30:39 -0700

    Remove external image domains to fix network error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['assets.fransium.com', 'images.unsplash.com', 'res.cloudinary.com'],
-  },
   experimental: {
     appDir: true,
   },
